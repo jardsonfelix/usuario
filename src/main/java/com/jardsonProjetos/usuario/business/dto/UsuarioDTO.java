b1b42b257d41c78323e14411dd349553c34cbfee
Jardson Felix <jardsonjfs32@gmail.com>
2026-09-16 15:28:50 -0300

    adicionado método que salva usuário

---
+++ src/main/java/com/jardsonProjetos/usuario/business/dto/UsuarioDTO.java
@@ -0,0 +1,21 @@
+package com.jardsonProjetos.usuario.business.dto;
+
+import com.jardsonProjetos.usuario.infrastructure.entity.Telefone;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UsuarioDTO {
+
+    private String nome;
+    private String email;
+    private String senha;
+    private List<EnderecoDTO> enderecos;
+    private List<TelefoneDTO>telefones;
+
+}
